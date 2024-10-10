8996202e2c8f4af183186e50c365247163f4c429
LucasMoreira1 <lucas_moreira@outlook.com>
2024-10-10 20:28:48 -0300

    correção type datatime. direto no connection string

--- CRUD.cs
+++ CRUD.cs
@@ -11,7 +11,7 @@ namespace Advocacia_Dias_Pereira
             //string conString = "Server=108.167.132.199;Database=soluc963_ADV_DIASPEREIRA;Uid=soluc963_DIASPER; Pwd=fXsI2J01!-5-;Convert Zero Datetime=True";
             //string conString = "Server=187.95.26.193;Port=3306;Database=soluc963_ADV_DIASPEREIRA;Uid=Agility;Pwd=!Parrudo292701;Convert Zero Datetime=True";
             //string conString = "Server=mysql-agility.advogadodigital.click;Port=3306;Database=soluc963_adv_diaspereira;Uid=Agility;Pwd=!Parrudo292701;";
-            string conString = "Server=192.168.1.75;Port=3306;Database=soluc963_adv_diaspereira;Uid=agility;Pwd=!Parrudo292701;";
+            string conString = "Server=192.168.1.75;Port=3306;Database=soluc963_adv_diaspereira;Uid=agility;Pwd=!Parrudo292701;Allow Zero Datetime=True;";
             return conString;
         }
 
